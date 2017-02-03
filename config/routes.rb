549addbc9890d2b2bb08b76e3e4fa5456966bbaa
dejo-olakareem <oladejoolakareem@yahoo.com>
2017-02-02 20:49:28 -0800

    Landing page and session new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :session
+  
+  resources :users
+  resources :spots
+  resources :reservations
+  get 'welcome/index'
+
+  root 'welcome#index'
+
+
 end
