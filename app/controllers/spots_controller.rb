be1feb63cf7cdb3fd6670fb2cfdf61ae4cbe7d9e
Jay Naidu <jnaidu@mail.sfsu.edu>
2017-02-03 16:33:21 -0800

    Create new spot form, add spots to database

---
+++ app/controllers/spots_controller.rb
@@ -0,0 +1,26 @@
+class SpotsController < ApplicationController
+
+  def create
+    @spot = Spot.new(spot_params)
+
+    # TODO: Replace placeholder user with current user
+    @spot.owner = User.first
+    
+    if @spot.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @spot = Spot.new
+  end
+
+private
+
+  def spot_params
+    params.require(:spot).permit(:address, :price, :end_time)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: "Jay", last_name: "Naidu", username: "jnaidu", phone: "111-111-1111", email: "jnaidu@mail.com", password: "password")
