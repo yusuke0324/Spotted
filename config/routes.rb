baaa11bd90e7a2668083cdbedc44a45fc02c2349
dejo-olakareem <oladejoolakareem@yahoo.com>
2017-02-03 16:52:03 -0800

    User log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :session
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html	
   resources :spots
   resources :reservations
   get 'welcome/index'
