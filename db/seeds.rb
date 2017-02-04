eb1d16502a6b0c1143cfcbf76f10c79fc81018f6
Jay Naidu <jnaidu@mail.sfsu.edu>
2017-02-03 17:52:23 -0800

    Add spots to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(first_name: "Jay", last_name: "Naidu", username: "jnaidu", phone: "111-111-1111", email: "jnaidu@mail.com", password: "password")
+user = User.create(first_name: "Jay", last_name: "Naidu", username: "jnaidu", phone: "111-111-1111", email: "jnaidu@mail.com", password: "password")
+
+Spot.create(address: "61 Oak Grove St", price: 10, end_time: "10:00", owner: user)
+Spot.create(address: "970 Howard St", price: 10, end_time: "9:00", owner: user)
+Spot.create(address: "833 Bryant St", price: 20, end_time: "11:00", owner: user)
