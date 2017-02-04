fe8a61de95665c6073e2898d367b9b3d2812bf12
dejo-olakareem <oladejoolakareem@yahoo.com>
2017-02-03 18:06:34 -0800

    Fully implement device

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html	
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+  # devise_for :users, :controllers => { registrations: 'users/registrations' }
+  # devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :spots
