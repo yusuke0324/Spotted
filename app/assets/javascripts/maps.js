3bcf11e20ce8b5360712e8cc677628eebbbb4863
nightangelblade <sean.syc.wright@gmail.com>
2017-02-03 17:58:33 -0800

    Testing HTML5 geolocation for markers in JavaScript.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   var map = new google.maps.Map(document.getElementById('map'), mapOptions);
   var marker = new google.maps.Marker(markerOptions);
 	marker.setMap(map);
+	
 });
 
 var mapOptions = {
@@ -13,3 +14,23 @@ var mapOptions = {
 var markerOptions = {
     position: new google.maps.LatLng(37.7819057,-122.4536707)
 };
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+  console.log (crd);
+  console.log(`${crd.latitude}`);
+  console.log(`${crd.longitude}`);
+  console.log(`${crd.accuracy}`);
+};
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
 	def show
+		@location = Geocoder.coordinates("173.46.64.174")
+		p @location
 		@user = User.find(params[:id])
 	end
 end
