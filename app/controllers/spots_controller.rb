e6ea900945641e2704efb49e4186b27e4343963b
dejo-olakareem <oladejoolakareem@yahoo.com>
2017-02-04 16:02:51 -0800

    css form for newspot

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,5 +1,7 @@
 class SpotsController < ApplicationController
 
+  # before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
+
   def create
     @spot = Spot.new(spot_params)
 
@@ -17,10 +19,36 @@ class SpotsController < ApplicationController
     @spot = Spot.new
   end
 
+  def edit
+    @spot = Spot.find(params[:id])
+  end
+
+  def show
+    @spot = Spot.find(params[:id])
+  end
+
+  def update
+    
+    @spot = Spot.find(params[:id]) #define variable to edit
+
+    @spot.assign_attributes(params[:spot]) #assign new attributes
+
+    if @spot.save 
+      redirect_to spot_path
+    else
+      render 'edit'
+    end
+
+  end
+
 private
 
   def spot_params
     params.require(:spot).permit(:address, :price, :end_time)
+  end
+
+  def set_s3_direct_post
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
   end
 
 end
