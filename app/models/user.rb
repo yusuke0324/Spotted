e08f77012acbc923c599362d4eafe287827e3f31
yusuke0324 <take.yusuke@gmail.com>
2017-02-05 12:56:54 -0800

    Add current_reservation to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   has_many :owned_spots, class_name:"Spot", foreign_key: "owner_id"
   has_many :reservations
   has_many :spots, through: :reservations
+  has_one :current_reservation, class_name: 'reservation'
+
 end
