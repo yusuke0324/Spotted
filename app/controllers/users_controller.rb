3af950838435fdabbf67fa1a076e0e37dfa7a275
Derek Pham <derekmpham@gmail.com>
2017-02-05 13:04:40 -0800

    Fix merge conflict pt 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
 	def show
-		@location = Geocoder.coordinates("173.46.64.174")
-		p @location
 		@user = User.find(params[:id])
 		@spots = @user.spots
+		if request.xhr?
+			latitude =  params[:latitude]
+			longitude = params[:longitude]
+			@parking_spots = Spot.near("#{latitude}, #{longitude}")
+			@parking_selections = []
+			@parking_spots.each {|spot| @parking_selections << {address: spot.address, latitude: spot.latitude, longitude: spot.longitude, price: spot.price}}
+			content_type = :json
+			{ parking: @parking_selections }.to_json
+		end
 	end
 end
