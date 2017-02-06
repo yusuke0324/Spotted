56f35240d251925a89b4f0e872dfbbe8ba1cff8f
nightangelblade <sean.syc.wright@gmail.com>
2017-02-06 09:23:26 -0800

    Edit reservation controller and user show view for updating reservation.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,7 +6,7 @@ class ReservationsController < ApplicationController
     @spot = Spot.find(params[:spot_id])
     @reservation = Reservation.create(spot: @spot, user: current_user, start_time: DateTime.now)
     current_user.current_reservation = @reservation
-    render 'uesrs/show'
+    render user_path(current_user)
   end
 
   # This is for 'finish' button when user want to terminate the reservation
@@ -14,6 +14,6 @@ class ReservationsController < ApplicationController
     @reservation = current_user.current_reservation
     @reservation.update(end_time: DateTime.now)
     current_user.current_reservation = nil
-    render 'user/show'
+    redirect_to user_path(current_user)
   end
 end
