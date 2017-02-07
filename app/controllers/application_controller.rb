e961effe84df63333a29396aa787987b6750d6c7
Derek Pham <derekmpham@gmail.com>
2017-02-06 21:44:37 -0800

    Have user redirected to users#show page after logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-   skip_before_filter :verify_authenticity_token  
+	protect_from_forgery with: :exception
+	skip_before_filter :verify_authenticity_token
+
+	def after_sign_in_path_for(resource)
+		user_path(current_user)
+	end  
 end
