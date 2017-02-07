299ea83ba07e4c03e1e96af34476313ef7b89240
nightangelblade <sean.syc.wright@gmail.com>
2017-02-07 09:44:02 -0800

    Edit with working toggle.

--- app/assets/javascripts/change.js
+++ app/assets/javascripts/change.js
@@ -7,9 +7,8 @@ $(document).on('turbolinks:load', function(){
 
 var swit = function(){
 	$("body").on("click", $("#right") ,function(e){
-		console.log("yeag")
-		$("#right").parent().siblings("div").toggleClass("owner_wants_to_rent")
-		$(".owner").hide()
-
+		$(this).find("#owner").show()
+		$("#renter").hide()
+		$("#right").hide()
 	})
 }
