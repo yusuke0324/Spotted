e5c9670fd96bb1f92b3a79613edf790223754485
nightangelblade <sean.syc.wright@gmail.com>
2017-02-07 12:49:21 -0800

    Parking spots dynamically updated.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,22 +11,21 @@ class UsersController < ApplicationController
 				gon.longitude = @location.longitude
 			else
 				gon.no_reservation = "none"
-
 			end
-			@spots = @user.owned_spots
 			if request.xhr?
 				latitude =  params[:latitude]
 				longitude = params[:longitude]
 				@parking_spots = Spot.near([latitude, longitude], 0.5).limit(4)
 				@parking_spots = @parking_spots.select{|spot| spot.availability == true}
-				p @parking_spots
+				@parking_display = render_to_string(partial: 'parking')
 				@parking_selections = []
 				@parking_spots.each {|spot| @parking_selections << {id: spot.id, address: spot.address, latitude: spot.latitude, longitude: spot.longitude, price: spot.price}}
 				respond_to do |format|
-					response = { :selections => @parking_selections}
+					response = { selections: @parking_selections, display: @parking_display}
 					format.json { render :json => response }
 				end
 			end
+			@spots = @user.owned_spots
 		else
 			redirect_to new_user_session_path
 		end
