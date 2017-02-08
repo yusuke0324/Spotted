2cc7acc56b873f30dcf2260c1bc4a49fc474f86c
dejo-olakareem <oladejoolakareem@yahoo.com>
2017-02-07 20:01:54 -0800

    resolve error for carousel

--- app/assets/javascripts/simple_slider.js
+++ app/assets/javascripts/simple_slider.js
@@ -1,18 +1,24 @@
 $( document ).on('turbolinks:load', function(){
-
 	var myIndex = 0;
+    carousel();
 
 	function carousel() {
     var i;
-    var x = document.getElementsByClassName("swiper-slide");
+    var x = $(".swiper-slide");
+    if (x.length ==0){
+        return false;
+    }
+    console.log(x)
     console.log(x.length);
     for (i = 0; i < x.length; i++) {
-       $(x[i]).attr("display", "none");  
+        console.log(x);
+       x[i].style.display = "none"; 
     }
     myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    $(x[myIndex-1]).attr("display", "block");  
+    if (myIndex > x.length) {myIndex = 1}
+
+    x[myIndex-1].style.display = "block";  
     setTimeout(carousel, 4000); // Change image every 2 seconds
 	}
-	carousel();
+	
 });
