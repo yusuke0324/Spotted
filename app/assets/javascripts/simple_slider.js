a04ff545ad09253bee3bd5b1af9013330f1dd813
dejo-olakareem <oladejoolakareem@yahoo.com>
2017-02-08 08:27:50 -0800

    Revert "resolve error for carousel"
    This reverts commit 2cc7acc56b873f30dcf2260c1bc4a49fc474f86c.

--- app/assets/javascripts/simple_slider.js
+++ app/assets/javascripts/simple_slider.js
@@ -1,24 +1,18 @@
 $( document ).on('turbolinks:load', function(){
+
 	var myIndex = 0;
-    carousel();
 
 	function carousel() {
     var i;
-    var x = $(".swiper-slide");
-    if (x.length ==0){
-        return false;
-    }
-    console.log(x)
+    var x = document.getElementsByClassName("swiper-slide");
     console.log(x.length);
     for (i = 0; i < x.length; i++) {
-        console.log(x);
-       x[i].style.display = "none"; 
+       $(x[i]).attr("display", "none");  
     }
     myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-
-    x[myIndex-1].style.display = "block";  
+    if (myIndex > x.length) {myIndex = 1}    
+    $(x[myIndex-1]).attr("display", "block");  
     setTimeout(carousel, 4000); // Change image every 2 seconds
 	}
-	
+	carousel();
 });
