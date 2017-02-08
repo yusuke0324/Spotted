24ad38afdfd62c5375ad3ef4f856c7dedcaa3dd4
dejo-olakareem <oladejoolakareem@yahoo.com>
2017-02-08 15:31:01 -0800

    WIP to send message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+	require 'twilio-ruby'
+	def send_sms(reservation)
+		account_sid=ENV["twilid"]
+		auth_token=ENV["twiltoken"]
+
+		@client = Twilio::REST::Client.new account_sid, auth_token
+
+		@client.messages.create(
+			  from: '+16125004243',
+			  to: '+1reservation.spot.owner.phone',
+			  body: 'reservation.spot.user.phone just reserved your spot!',
+			  media_url: 'http://www.drodd.com/images12/smiley-face-clip-art15.jpg'
+			)
+
+	end
 end
