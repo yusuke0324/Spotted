95a86cda47001fb42d2adfbd7a07edb14bda8891
Derek Pham <derekmpham@gmail.com>
2017-02-08 17:54:43 -0800

    Add more photos for slides

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,32 +1,3 @@
 $(document).on('turbolinks:load', function() {
 	$('.single-item').slick({});
 })
-// $(document).on('turbolinks:before-cache', $('.single-item').slick('unslick'))
-// $('.single-item').slick();
-
-// $(function(){
-
-//    // Initializing the swiper plugin for the slider.
-//     // Read more here: http://idangero.us/swiper/api/
-//     var mySwiper = new Swiper ('.swiper-container', {
-//         loop: true,
-//         pagination: '.swiper-pagination',
-//         paginationClickable: true,
-//         nextButton: '.swiper-button-next',
-//         prevButton: '.swiper-button-prev'
-//     });
-    
-//     mySwiper.stopAutoplay();
-
-// });
-
-// $(document).ready(function () {
-//   //initialize swiper when document ready  
-//     var mySwiper = new Swiper ('.swiper-container', {
-//         loop: true,
-//         pagination: '.swiper-pagination',
-//         paginationClickable: true,
-//         nextButton: '.swiper-button-next',
-//         prevButton: '.swiper-button-prev'
-//     });     
-// });
